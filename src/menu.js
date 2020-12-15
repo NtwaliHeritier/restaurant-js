b7e3c2915675cf3cb0a81e5908d6bbe26a37dc96
NtwaliHeritier <heritierntwali@gmail.com>
2020-12-15 23:21:34 +0200

    add content on menu page

--- src/menu.js
+++ src/menu.js
@@ -6,11 +6,26 @@ const Menu = (()=>{
         menu.setAttribute('id', 'menupage')
         menu.innerHTML = `
         <div class="breakfast-menu">
-        <h3>Breakfast</h3>
-        <span>Omelette</span>
-        <span>Coffee</span>
-        <span>Tea</span>
-        <span>Bread</span>
+          <h3>Breakfast</h3>
+          <span>Omelette</span>
+          <span>Coffee</span>
+          <span>Tea</span>
+          <span>Bread</span>
+        </div>
+        <div class="lunch-menu">
+          <h3>Lunch</h3>
+          <span>Burger</span>
+          <span>Pizza</span>
+          <span>Steak</span>
+          <span>Chicken panini</span>
+        </div>
+        <div class="dinner-menu">
+          <h3>Dinner</h3>
+          <span>Salad</span>
+          <span>Brown bread</span>
+          <span>Soup</span>
+          <span>White bread</span>
+        </div>
         `
         const container = document.getElementById('content')
         container.appendChild(menu)
